3c25d3998174b91dad12ef9974d50c2cbdb49d22
Hepestoo <teckalan2@gmail.com>
2025-10-23 07:59:42 -0500

    Segundo commit: test con error intencional

--- tests/calcUtils.test.js
+++ tests/calcUtils.test.js
@@ -1,6 +1,6 @@
 const { calculate } = require('../src/utils/calcUtils');
 
-test('suma correctamente', () => expect(calculate(2,3,'+')).toBe(5));
+test('suma correctamente (fallará)', () => expect(calculate(2,3,'+')).toBe(6));
 test('resta correctamente', () => expect(calculate(7,4,'-')).toBe(3));
 test('lanza error con operador inválido', () => expect(() => calculate(2,2,'x')).toThrow('Invalid operator'));
 test('lanza error al dividir entre cero', () => expect(() => calculate(5,0,'/')).toThrow('Division by zero'));
